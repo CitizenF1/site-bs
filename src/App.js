448513bb9409815be595b976a5d6377ba51e66ce
Azamat <AXpert22@gmail.com>
2021-01-24 18:26:30 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Button variant="primary">Web </Button>
+  );
+}
+
+export default App;
